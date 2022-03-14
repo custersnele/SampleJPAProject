46880cf3c411f77809bbf9ba99d73d8aa3c4be9f
Nele Custers <custersnele@gmail.com>
2022-03-14 10:56:07 +0100

    add equals method

--- src/main/java/be/pxl/paj/domain/Project.java
+++ src/main/java/be/pxl/paj/domain/Project.java
@@ -55,6 +55,25 @@ public class Project {
 		return researchers;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Project project = (Project) o;
+
+		return id != null ? id.equals(project.id) : project.id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
 	public void addResearcher(Researcher researcher) {
 		researchers.add(researcher);
 	}
